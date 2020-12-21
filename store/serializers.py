67c408a2b0db2139c154d3d38e2cbe61167c1f25
Sergey Richkov <sergey.rychkov.2016@gmail.com>
2020-12-22 06:46:29 +0700

    second commit

--- store/serializers.py
+++ store/serializers.py
@@ -1,22 +1,30 @@
+from abc import ABC
+
 from rest_framework import serializers, viewsets
 from store.models import Sales
 
 
-class DualSerializerViewSet(viewsets.ModelViewSet):
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return serializers.SalesSerializer
-        if self.action == 'list':
-            return serializers.DettaglioGruppi
-
-
-
-
-
 class SalesSerializer(serializers.ModelSerializer):
-    """Serializer для клиентов."""
+    """Serializer для продаж."""
 
     class Meta:
         model = Sales
         fields = ('customer', 'item', 'total', 'quantity', 'date')
 
+
+class CustomerSerializer(serializers.Serializer):
+    spent_money = serializers.FloatField()
+    username = serializers.CharField()
+    gems = serializers.ListField()
+
+
+class CustomersSerializer(serializers.Serializer):
+    response = serializers.ListField(child=CustomerSerializer())
+
+
+class DualSerializerViewSet(viewsets.ModelViewSet):
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return SalesSerializer
+        if self.action == 'list':
+            return CustomersSerializer
--- store/views.py
+++ store/views.py
@@ -1,77 +1,74 @@
 import csv
 from django.db import connection
-from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from store.models import Sales
-from store.serializers import SalesSerializer
+from store.serializers import DualSerializerViewSet
 
 
 class SalesViewSet(ModelViewSet):
     """ ViewSet для продаж """
-
     queryset = Sales.objects.all()
-    serializer_class = SalesSerializer
-
-
-
+    serializer_class = DualSerializerViewSet.get_serializer_class()
 
     def create(self, request):
-        cursor = connection.cursor()
-        cursor.execute("TRUNCATE TABLE store_sales RESTART IDENTITY")
-        file = request.GET.get('filename')
-        with open(file, 'r', encoding='utf-8') as csvfile:
-            deals_reader = csv.reader(csvfile, delimiter=',')
-            next(deals_reader)
+        try:
+            cursor = connection.cursor()
+            cursor.execute("TRUNCATE TABLE store_sales RESTART IDENTITY")
+            file = request.GET.get('filename')
+            with open(file, 'r', encoding='utf-8') as csvfile:
+                deals_reader = csv.reader(csvfile, delimiter=',')
+                next(deals_reader)
 
-            for line in deals_reader:
-                Sales.objects.create(customer=line[0], item=line[1],
-                                     total=float(line[2]),
-                                     quantity=line[3],
-                                     date=line[4],
-                                     )
-        print('Kavabanga!!')
-        return Response('hgvjghv')
+                for line in deals_reader:
+                    Sales.objects.create(customer=line[0],
+                                         item=line[1],
+                                         total=float(line[2]),
+                                         quantity=line[3],
+                                         date=line[4],
+                                         )
 
-    # def retrieve(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     serializer = self.get_serializer(instance)
-    #     # print(serializer.data)
-    #     return Response({'response': '' }, status.HTTP_205_RESET_CONTENT)
-    #
-    #
+            return Response({'Status': "ОК"}, status.HTTP_201_CREATED)
+
+        except:
+            return Response({'Status': 'Error', 'Desc': status.HTTP_400_BAD_REQUEST},
+                            status.HTTP_400_BAD_REQUEST)
+
+
     def list(self, request, *args, **kwargs):
-        kk = []
-        pp = []
-        summ = 0
         queryset = self.filter_queryset(self.get_queryset())
-        customer_queryset = queryset.distinct('customer')
-        serializer = self.get_serializer(queryset, many=True)
-        serializer1 = self.get_serializer(customer_queryset, many=True)
 
-        for k in serializer1.data:
-            kk.append(k['customer'])
-        for customer in kk:
+        all_customer = [k['customer'] for k in queryset.values()]
+
+        customer_spent = []
+        for customer in set(all_customer,):
+            summ = 0
             for quer in queryset:
                 if customer == quer.customer:
                     summ += quer.total
-            pp.append([customer, summ])
-        pp.sort(key=lambda i: i[1], reverse=True)
-        print(pp[0:5])
-        gg = []
-        kamni = []
-        hhhhh = pp[0:5]
-        for cus in hhhhh:
-            gg.append(queryset.filter(customer=cus[0]).values('item').distinct())
-            kamni.extend(queryset.filter(customer=cus[0]).values('item').distinct())
+            customer_spent.append([customer, summ, []])
+        customer_spent.sort(key=lambda i: i[1], reverse=True)
 
-        for i, qq in enumerate(gg):
-            for qqq in qq:
-                if kamni.count(qqq) > 1:
-                    hhhhh[i].append(qqq['item'])
+        each_customers_gems = []
+        all_gems_set = []
 
-        return Response(hhhhh, status.HTTP_205_RESET_CONTENT)
+        five_largest_spending = customer_spent[0:5]
+        for customer in five_largest_spending:
+            each_customers_gems.append(queryset.filter(customer=customer[0]).values('item').distinct())
+            all_gems_set.extend(queryset.filter(customer=customer[0]).values('item').distinct())
+
+        for i, gems in enumerate(each_customers_gems):
+            for gem in gems:
+                if all_gems_set.count(gem) > 1:
+                    five_largest_spending[i][2].append(gem['item'])
+
+        response_list = []
+        for zz in five_largest_spending:
+            response_list.append(dict(zip(['username', 'spent_money', 'gems'], zz)))
+
+        return Response({'response': response_list}, status.HTTP_201_CREATED)
+
 
 
 
